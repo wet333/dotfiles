804805071a84b66b80a320d93a19eb4f5422594f
wet333 <wet.4gustin@gmail.com>
2024-05-29 01:07:12 -0300

    Modified fs_init.sh, now each folder also creates a symlink in the home directory to prevent file explorers to crash when you click default folders like Music or Documents

--- system/files/fs_init.sh
+++ system/files/fs_init.sh
@@ -5,28 +5,29 @@
 
 # Directory Lists
 main_folder="Files"
-folders_to_create=("Downloads" "Images" "Music" "Programming" "Docs")
-folders_to_delete=("Downloads" "Music" "Pictures" "Videos" "Templates" "Public" "Documents")
+folders_to_create=("Downloads" "Music" "Pictures" "Videos" "Programming" "Documents")
+folders_to_delete=("Downloads" "Music" "Pictures" "Videos" "Documents" "Templates" "Public")
 
 # Init Configuration
 DELETE_FOLDERS=true
 
 # Functions ------------------------------------------------------------------------------------------------------------
 fs_init() {
-    create_folders
-
     if $DELETE_FOLDERS; then
-        delete_folders
+          delete_folders
     else
         echo "Delete operation skipped. To delete default directories, Set DELETE_FOLDERS to true."
     fi
+
+    create_folders
 }
 
 create_folders() {
     for folder in "${folders_to_create[@]}"; do
         mkdir -p ~/"$main_folder"/"$folder"
+        ln -s ~/"$main_folder"/"$folder" ~/"$folder"
     done
-    echo "Folders created successfully."
+    echo "Folders created and symlinks added successfully."
 }
 
 delete_folders() {
