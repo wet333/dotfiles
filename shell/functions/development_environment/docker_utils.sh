18688b537f331cdcf2f022c6f9bc28731dffcc19
wet333 <wet.4gustin@gmail.com>
2025-05-02 03:13:58 -0300

    delete docker function, sudo group.

--- shell/functions/development_environment/docker_utils.sh
+++ shell/functions/development_environment/docker_utils.sh
@@ -5,12 +5,6 @@ docker_get_ip() {
     docker network inspect bridge | grep Gateway | sed 's/^[ \t]*//;s/[ \t]*$//'
 }
 
-docker_config_no_sudo() {
-    sudo groupadd docker
-    sudo usermod -aG docker "$USER"
-    sudo systemctl restart docker
-}
-
 # Removes all dangling images from Docker
 docker_rmi_dangling() {
     if [[ $(docker images -f "dangling=true" -q) ]]; then
